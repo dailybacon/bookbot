937721f4e9bfd88ce8c2b7445806ff42a0df874b
dailybacon <themutie@gmail.com>
2024-12-28 12:02:25 -0600

    updated readme, added comments

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ def main():
     
     print_report(file_path, word_count, list_of_dicts)
 
+# reads file into string
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+# returns word count of book
 def get_word_count(text):
     text = text.split()
     return len(text)
 
+# creates a dictionary key for every unique character in book and tallies the amount of said characters for the value
 def count_characters(words):
     char_dictionary = {}
-    text_string = str(words.lower())
+    words = str(words.lower()) # makes all words lowercase
 
-    for letter in text_string:
+    for letter in words:
         if letter in char_dictionary:
             char_dictionary[letter] += 1
         else:
@@ -31,6 +34,7 @@ def count_characters(words):
 def sort_on(dict):
     return next(iter(dict.values()))
 
+#creates a list of dictionaries of only alphabetic characters
 def list_character_count(char_dict):
     dict_to_list = []
     for key, value in char_dict.items():
@@ -39,6 +43,7 @@ def list_character_count(char_dict):
     dict_to_list.sort(reverse=True, key=sort_on)
     return dict_to_list
 
+#generates the report seen when you execute this file
 def print_report(file, no_of_words, list_of_dicts):
     print(f'--- Begin report of {file} ---')
     print(f"{no_of_words} words found in the document.\n")
@@ -46,6 +51,6 @@ def print_report(file, no_of_words, list_of_dicts):
         for k in i:
             print(f'There are {i[k]} occurences of the letter \'{k}\'. ')
         
-    print(f'--- End report ---')
+    print(f'\n--- End report ---')
 
 main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# bookbot
+
+BookBot is my first project!  It's the first guided projct on [boot.dev](https://www.boot.dev).  Check it out!
